12abf78acd19f7b966ed97563caa839d5ff48f3a
Subrota Chandra Sarker <itinfobd24@gmail.com>
2022-12-30 21:47:21 +0600

    add all updated files

--- src/app.js
+++ src/app.js
@@ -3,15 +3,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken") ;
 const port = process.env.PORT || 3024;
 //db
-const { MongoClient, ServerApiVersion, Collection, ObjectId, Double } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, Double } = require('mongodb');
 const uri = process.env.mongodb_url;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
+//verify token
+const verifyToken = (req , res , next ) => {
+const token = req.headers.authentication ; 
+const getToken = token.split(" ")[1] ;
+jwt.verify(getToken , process.env.scure_token , function(error , decoded){
+if(error){
+return res.status(403).send({message:"unauthorize access"}) ;
+}
+req.decoded = decoded ; 
+next() ;
+})
+}   
 const runMongoDB = async () => {
   try {
     //collections
@@ -22,32 +35,32 @@ const runMongoDB = async () => {
     const commentCollection = client.db("socialMedia").collection("comment");
     const aboutCollection = client.db("socialMedia").collection("about");
 
-    app.post("/posts", async (req, res) => {
+    app.post("/posts"  , verifyToken ,  async (req, res) => {
       const postData = req.body;
       const insertData = await postCollection.insertOne(postData);
       res.status(201).send(insertData);
     });
 
-    app.get("/posts", async (req, res) => {
+    app.get("/posts"  ,  async (req, res) => {
       const getData = await postCollection.find().toArray();
       res.status(201).send(getData);
     });
 
-    app.get("/posts/:id", async (req, res) => {
+    app.get("/posts/:id" , verifyToken,   async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const postData = await postCollection.findOne(filter);
       res.status(201).send(postData);
     })
 
-    app.get("/topPosts", async (req, res) => {
+    app.get("/topPosts" ,  async (req, res) => {
       const topPostsData = await postCollection.find().sort({ likes: -1 }).limit(3).toArray();
       res.status(201).send(topPostsData);
     })
 
     //>>---------------->> Post like start <<---------------<<
 
-    app.put("/postLike", async (req, res) => {
+    app.put("/postLike", verifyToken ,  async (req, res) => {
       let postId = req.query.postId;
       let likerData = req.body;
       let filter = { _id: ObjectId(postId) };
@@ -80,7 +93,7 @@ const runMongoDB = async () => {
 
     //>>---------------->> Post postWithdrawLike start <<---------------<<
 
-    app.put("/postWithdrawLike", async (req, res) => {
+    app.put("/postWithdrawLike", verifyToken ,  async (req, res) => {
       let postId = req.query.postId;
       let filter = { _id: ObjectId(postId) };
       let likeFind = await postCollection.findOne(filter);
@@ -104,7 +117,7 @@ const runMongoDB = async () => {
 
     //>>---------------->> Post dislike start <<---------------<<
 
-    app.put("/postDisLike", async (req, res) => {
+    app.put("/postDisLike", verifyToken ,  async (req, res) => {
       let postId = req.query.postId;
       let disLikerData = req.body;
       let filter = { _id: ObjectId(postId) };
@@ -138,7 +151,7 @@ const runMongoDB = async () => {
 
     //>>---------------->> Post postWithdrawDisike start <<---------------<<
 
-    app.put("/postWithdrawDisike", async (req, res) => {
+    app.put("/postWithdrawDisike", verifyToken ,  async (req, res) => {
       let postId = req.query.postId;
 
       let filter = { _id: ObjectId(postId) };
@@ -163,7 +176,7 @@ const runMongoDB = async () => {
 
 
     //get liker data
-    app.get("/likerData", async (req, res) => {
+    app.get("/likerData", verifyToken ,  async (req, res) => {
       const email = req.query.email;
       const id = req.query.id;
       const filter = { $and: [{ likerEmail: email }, { likeId: id }] };
@@ -172,7 +185,7 @@ const runMongoDB = async () => {
     })
 
     //get disliker data
-    app.get("/dislikerData", async (req, res) => {
+    app.get("/dislikerData", verifyToken ,  async (req, res) => {
       const email = req.query.email;
       const id = req.query.id;
       const filter = { $and: [{ dislikerEmail: email }, { dislikeId: id }] };
@@ -182,20 +195,20 @@ const runMongoDB = async () => {
 
     //insert user informations
 
-    app.post("/users", async (req, res) => {
+    app.post("/users", verifyToken ,  async (req, res) => {
       const userPostData = req.body;
       const insertUsers = await usersCollection.insertOne(userPostData);
       res.status(201).send(insertUsers);
     });
     //save comment
 
-    app.post("/comments", async (req, res) => {
+    app.post("/comments", verifyToken ,  async (req, res) => {
       const commentsData = req.body;
       const result = await commentCollection.insertOne(commentsData);
       res.status(201).send(result);
     })
     //get comments
-    app.get("/comments/:id", async (req, res) => {
+    app.get("/comments/:id" ,  async (req, res) => {
       const id = req.params.id;
       const filter = { postId: id };
       const result = await commentCollection.find(filter).sort({ _id: -1 }).toArray();
@@ -203,13 +216,13 @@ const runMongoDB = async () => {
     })
 
     //get about information
-    app.get("/about", async (req, res) => {
+    app.get("/about", verifyToken ,  async (req, res) => {
       const result = await aboutCollection.find().toArray();
       res.status(201).send(result);
     });
     //get one about information
 
-    app.get("/about/:id", async (req, res) => {
+    app.get("/about/:id", verifyToken ,  async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const result = await aboutCollection.findOne(filter);
@@ -218,7 +231,7 @@ const runMongoDB = async () => {
 
     //update one about information
 
-    app.put("/update-about-information/:id", async (req, res) => {
+    app.put("/update-about-information/:id", verifyToken ,  async (req, res) => {
       const id = req.params.id;
       const aboutUpdateInfo = req.body;
       const { name, email, address, university, profession, profile } = aboutUpdateInfo;
@@ -240,7 +253,13 @@ const runMongoDB = async () => {
       //  console.log(result);
       res.status(201).send(result);
     })
-
+//generate a token
+    app.post("/jwt"  ,  async(req , res) => {
+      const email = req.body ;
+      const scure_token = process.env.scure_token;
+      const token = jwt.sign(email , scure_token  , {expiresIn:"2d"})  ;
+      res.status(201).send({token:token}) ;
+    })
     //----??
   } catch (error) {
     console.log(error);
